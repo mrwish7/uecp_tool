07ed178901150a9646cb7f0308aec34e55584487
mrwish7 <37659188+mrwish7@users.noreply.github.com>
2018-10-20 14:28:58 +0100

    Update uecp_tool.py

--- uecp_tool.py
+++ uecp_tool.py
@@ -1,3 +1,30 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Adam Wisher
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in
+#    all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#    THE SOFTWARE.
+#
+# Using some code by Matthias P. Braendli - https://github.com/mpbraendli/mmbtools-aux for some UECP decoding functions.
+#
+# Decode UECP information from a 48kHz sample rate MP2 PES stream
+# Usage: uecp_tool.py <MP2 stream bitrate> e.g. "uecp_tool.py 320" for a 320kbps stream.
+
 import sys
 
 ps = None
